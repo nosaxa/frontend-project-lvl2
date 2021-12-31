d8d47849303554c423763383728633c39d6511fd
Maxim <nosaxa@protonmail.com>
2021-12-31 08:23:10 +0100

    Create function which get the full path to a file

---
+++ src/getFixturePath.js
@@ -0,0 +1,12 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFixturePath = (fileName) => {
+  const fixturePath = path.join(__dirname, '..', '__fixtures__', fileName);
+  return fixturePath;
+};
+
+export default getFixturePath;
