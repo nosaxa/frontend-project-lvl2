31b82fe163744e3dcd36c24cbf064184b59e7d5e
Maxim <nosaxa@protonmail.com>
2022-01-16 11:16:22 +0100

    Create function that finds difference

---
+++ src/buildDiffTree.js
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+const buildDiffTree = (data1, data2) => {
+  const keys = _.union(_.keys(data1), _.keys(data2));
+
+  return keys.map((key) => {
+    const value1 = data1[key];
+    const value2 = data2[key];
+
+    if (!_.has(data2, key)) {
+      return { key, status: 'removed', value: value1 };
+    }
+
+    if (!_.has(data1, key)) {
+      return { key, status: 'added', value: value2 };
+    }
+
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
+      return { key, status: 'nested', children: buildDiffTree(value1, value2) };
+    }
+
+    if (_.isEqual(value1, value2)) {
+      return { key, status: 'unchanged', value: value1 };
+    }
+
+    return {
+      key,
+      status: 'changed',
+      before: value1,
+      after: value2,
+    };
+  });
+};
+
+export default buildDiffTree;
