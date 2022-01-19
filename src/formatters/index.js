25a0d5d945883e142f6beb3bd87998c5133f5f1d
Maxim <nosaxa@protonmail.com>
2022-01-19 17:36:41 +0100

    Create main module for different formatters

---
+++ src/formatters/index.js
@@ -0,0 +1,18 @@
+import makeStylish from './stylish.js';
+import makePlain from './plain.js';
+
+const getFormat = (tree, formatName) => {
+  if (formatName === 'stylish') {
+    return makeStylish(tree);
+  }
+
+  if (formatName === 'plain') {
+    return makePlain(tree);
+  }
+
+  if (formatName === 'json') {
+    return JSON.stringify(tree, null, 2);
+  }
+};
+
+export default getFormat;
