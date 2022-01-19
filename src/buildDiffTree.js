57d7ee3c4de51854374cc05779e67716c5b596af
Maxim <nosaxa@protonmail.com>
2022-01-19 07:35:33 +0100

    Make code more cleaner

--- src/buildDiffTree.js
+++ src/buildDiffTree.js
@@ -1,18 +1,18 @@
 import _ from 'lodash';
 
 const buildDiffTree = (data1, data2) => {
-  const keys = _.union(_.keys(data1), _.keys(data2));
+  const keys = _.sortBy(_.union(_.keys(data1), _.keys(data2)));
 
   return keys.map((key) => {
     const value1 = data1[key];
     const value2 = data2[key];
 
-    if (!_.has(data2, key)) {
-      return { key, status: 'removed', value: value1 };
-    }
-
     if (!_.has(data1, key)) {
       return { key, status: 'added', value: value2 };
+    }
+
+    if (!_.has(data2, key)) {
+      return { key, status: 'deleted', value: value1 };
     }
 
     if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
@@ -26,8 +26,8 @@ const buildDiffTree = (data1, data2) => {
     return {
       key,
       status: 'changed',
-      before: value1,
-      after: value2,
+      oldValue: value1,
+      newValue: value2,
     };
   });
 };
