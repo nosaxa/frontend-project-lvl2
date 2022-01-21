93300d0221e40614cb7149dd844e6552da80ba7a
Maxim <nosaxa@protonmail.com>
2022-01-21 19:46:51 +0100

    Include small splitted  modules into one

--- src/compareFiles.js
+++ src/compareFiles.js
@@ -1,15 +1,30 @@
+import path from 'path';
+import * as fs from 'fs';
+import { fileURLToPath } from 'url';
 import parseFile from './parser.js';
 import buildDiffTree from './buildDiffTree.js';
 import makeFormattedOutput from './formatters/index.js';
 
-const compareFiles = (file1, file2, format = 'stylish') => {
-  if (!parseFile(file1) || !parseFile(file2)) return null;
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-  const obj1 = parseFile(file1);
-  const obj2 = parseFile(file2);
+const getFixturePath = (fileName) => {
+  const fixturePath = path.join(__dirname, '..', '__fixtures__', fileName);
+  return fixturePath;
+};
+
+const getExtension = (filePath) => path.extname(filePath).slice(1);
+
+const compareFiles = (file1, file2, format = 'stylish') => {
+  const fileContent1 = fs.readFileSync(getFixturePath(file1));
+  const fileContent2 = fs.readFileSync(getFixturePath(file2));
+
+  const obj1 = parseFile(fileContent1, getExtension(file1));
+  const obj2 = parseFile(fileContent2, getExtension(file2));
+
   const tree = buildDiffTree(obj1, obj2);
 
   const result = makeFormattedOutput(tree, format);
+
   return result;
 };
 
